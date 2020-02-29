429c2bfd73b04131ca4ec7050dc1b3b95071f1de
miksiwgit <miksiwgit@gmail.com>
2020-02-29 18:32:11 +0100

    chandsagd in main cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,15 @@
 #include <iostream>
 
 int main() {
+    std::cout << "asdfasdfasdfsadfasdfasdf!" << std::endl;
+    std::cout << "asdfasdfasdfsadfasdfasdf!" << std::endl;
+    std::cout << "asdfasdfasdfsadfasdfasdf!" << std::endl;std::cout << "asdfasdfasdfsadfasdfasdf!" << std::endl;
+
+
+
+
+
+
     std::cout << "Hello, World!" << std::endl;
     return 0;
 }
